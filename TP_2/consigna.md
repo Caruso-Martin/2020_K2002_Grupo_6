52f60cd2a5882f14b4bc1b9283e7a3130a650ff5
Martin Caruso (Desktop) <martinpepa39@gmail.com>
2020-05-01 22:38:25 -0300

    VERSION 1 (1/5)
    #Actualizaciones
    ##procesarCadena( )
    ##ubicacionError( )
    ##tablaDeMovimiento**
    
    #Probar correcto funcionamiento
    ##procesarCadena( )
    ##ubicacionError( )
    
    #A realizar
    ##automataFinitoPila( )
    ##push( )
    ##pop( )

--- TP_2/consigna.md
+++ TP_2/consigna.md
@@ -6,7 +6,7 @@
 
 **Implementar en C** un programa que permita reconocer ​**expresiones aritméticas de suma, resta, producto y cociente** ​. El mismo deberá tomar una expresión (cadena de caracteres) **ingresada por el usuario** ​desde la entrada estándar (STDIN) y deberá indicar **por pantalla** (STDOUT) si la expresión es sintácticamente correcta o no. Dicho proceso se deberá poder repetir para cada cadena que vaya ingresando el usuario, siempre de a una cadena por vez.
 
-El programa debe contemplar la ​**implementación de un autómata finito de pila​**, pueden utilizar como referencia en AFP visto en clase con la siguiente tabla de transiciones (también conocida como tabla de movimientos).
+El programa debe contemplar la ​**implementación de un autómata finito de pila​**, pueden utilizar como referencia el AFP visto en clase con la siguiente tabla de transiciones (también conocida como tabla de movimientos).
 
 |    **TT**    |    0    |  [1-9]  | {+, -, *, /} |    (     |         )        |  
 |    :---:     |  :---:  |  :---:  |     :---:    |   :---:  |       :---:      |
@@ -21,7 +21,7 @@ El programa debe contemplar la ​**implementación de un autómata finito de pi
 Epsilon hace referencia a que no realizan ningún push sobre la pila. Recuerden que siempre se realiza el pop del símbolo de la cima de la pila. El push si es opcional.
 
 El alfabeto de cual podrán estar compuestas las expresiones (cadena de caracteres) son:
-$\sum$ = { 0,1,2,3,4,5,6,7,8,9,+,-,*,/,(,) }
+$\sum$ = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, +, -, *, /, ( , ) }
 
 El alfabeto de la pila será: $\sum$ **’** = {R} ,
 El símbolo de pila vacía será: p0 = $
--- TP_2/script.c
+++ TP_2/script.c
@@ -7,6 +7,86 @@
 #include <stdlib.h>
 #include <string.h>
 
-void main(){
+#define RESET           "\033[0m"
+#define RED             "\033[31m" 
+#define largoExpresion  100
 
+struct estadoTabla {
+    int estadoSiguiente; 
+    char cadenaPush[3];
+};
+
+void procesarCadena(char[]);
+void push();
+void pop();
+int automataFinitoPila(char[]);
+void ubicacionError(char[], int); 
+
+void main(){
+    char expresion[largoExpresion]; 
+
+    while (expresion != "@"){ // @ es un valor sentinela
+        procesarCadena(expresion);
+        int esSintacticamenteCorrecta = automataFinitoPila(expresion);
+
+        if(!esSintacticamenteCorrecta){
+            printf("La expresion ingresada NO es sintacticamente correcta");
+            //ubicacionError(expresion[], esSintacticamenteCorrecta); 
+        } else {
+            printf("La expresion ingresada es sintacticamente correcta");
+        }
+
+    }
+}
+void procesarCadena(char expresion[]){
+    int i = 0, j = 0; 
+    expresion[largoExpresion] = "";
+    printf("Ingrese una expresion: ");
+    scanf("%s\0", expresion);
+
+    while(expresion[i] != '\0'){
+        if(expresion[i] == " "){
+            while(expresion[j] != '\0'){
+                expresion[j] = expresion[j+1];
+                j++;
+            }
+        }
+        i++;
+        j = i;
+    }
+}
+void push(){
+    // Relacionado con (
+    // En tabla son las posiciones [0][3] para primer (; [2][3] para segundo o superior
+
+}
+void pop(){
+    // Relacionado con )
+    // En tabla son las posiciones [3][4] para primer ); [4][4] para segundo o superior
+
+}
+int automataFinitoPila(char expresion[]){ //Devuelve la posicion del primer error encontrado; si no hay, devuelve 0
+    int estadoActual = 0, columnaCaracter = 0; 
+    struct estadoTabla tablaDeMovimiento[6][5][1] = {
+        {{-1},  { 1},   {-1},  { 2},   {-1}},
+        {{ 1},  { 1},   { 0},  {-1},   {-1}},
+        {{-1},  { 3},   {-1},  { 2},   {-1}},
+        {{ 4},  { 4},   { 3},  {-1},   { 5}},
+        {{-1},  {-1},   { 3},  {-1},   { 5}},
+        {{-1},  {-1},   { 0},  {-1},   {-1}} 
+    }; //DE QUE TIPO ES LA TABLA
+
+
+}
+void ubicacionError(char expresion[], int posicionError){
+    int i = 0;
+    printf("El error se encontro en la posicion %d: ", posicionError);
+    while(expresion[i] != '\0'){
+        if(expresion[i] != posicionError){
+            printf("%c", expresion[i]);
+        } else {
+            printf(RED "%c" RESET, expresion[i]);
+        }
+    }
+    //Uso de color: https://stackoverflow.com/questions/1961209/making-some-text-in-printf-appear-in-green-and-red
 }
